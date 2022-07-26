48fc06526ab7c72c7e68c5c5ac20df08b1ace921
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-26 22:59:41 +0300

    Add content of Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+    <nav className='navBar'>
+      <div className='navbar-container'>
+       
+      </div>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar;
