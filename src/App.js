c0fd7c98a629aa73f85a6d6cd67d7b359251474f
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-27 01:08:49 +0300

    Add Businesspage component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Entrepreneur from './components/Entrepreneur';
 import Consultant from './components/Consultant';
 import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
+import BusinessPage from './components/BusinessPage';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <BusinessPage />
      <Entrepreneur />
      <Consultant />
      <SignUp />
---
+++ src/components/BusinessPage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function BusinessPage() {
+  return (
+    <div>
+    
+        <div className="businessName">
+          <h1>Nijenge</h1>
+          <div className="target">
+           <h4 id="entreBtn"><a href="#">Entrepreneur</a></h4>
+           <h4 id="investBtn"><a href="#">Investor</a></h4>
+           <h4 id="ConsulBtn"><a href="#">Consultants</a></h4>
+           <h4 id="followBtn"><a href="#">Follow Ups</a></h4>
+          </div>
+        </div>
+     
+      
+       <div className="navMenus">
+
+         <div>
+           <h3>Home</h3>
+           <h3 id="aboutLink">About</h3>
+           <h3>Services</h3>
+           <h3>Contact Us</h3>
+         </div>
+     
+       </div>
+      
+      </div>
+
+    
+
+
+  )
+}
+
+export default BusinessPage
