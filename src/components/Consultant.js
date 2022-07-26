562fdd42eadb14e5eeed11b114291089f4d17c78
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-26 22:53:07 +0300

    Add Consultants content

---
+++ src/components/Consultant.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Farming from './farming.jpg'
+
+
+function Consultant() {
+  return (
+    <div>
+
+      <h1>Consultants</h1>
+      <div className='consultants'>
+       
+        <img src={Farming} height="200px" width="300px"/>
+        <h2>Name</h2>
+        <p>Profession</p>
+        <p>Experience</p>
+        <p>Organization</p>
+
+      </div>
+      <div className='consultants'>
+        <img src={Farming} height="200px" width="300px"/>
+        <h2>Name: Caleb Karuga</h2>
+        <p>Profession: Farming</p>
+        <p>Experience: in both animal and crop farming</p>
+        <p>Organization: Wendy Farm</p>
+
+      </div>
+      <div className='consultants'>
+      <img src={Farming} height="200px" width="300px"/>
+      <h2>Name</h2>
+      <p>Profession</p>
+      <p>Experience</p>
+      <p>Organization</p>
+
+    </div>
+    </div>
+  )
+}
+
+
+export default Consultantc
