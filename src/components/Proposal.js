676ab196d9df34af3e464ac1af90f91e814df7cd
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-26 23:00:39 +0300

    Add content of Proposal component

---
+++ src/components/Proposal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Proposal({files, setFiles}) {
+
+  function handleUpload (){
+
+  }
+ 
+
+  return (
+    <section>
+      <div className='fileLoader'>
+        <div className='fileInputs'>
+          <input type='file' />
+                   
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Proposal;
