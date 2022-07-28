306f996f713456d9e872fe2229de5e875adc8cb0
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-28 04:18:32 +0300

    Change content in the App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import Entrepreneur from './components/Entrepreneur';
-import Consultant from './components/Consultant';
-import SignUp from './components/SignUp';
-import Navbar from './components/Navbar';
-import BusinessPage from './components/BusinessPage';
-import About from './components/About';
-import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import Main from './components/Main'
+
 
 function App() {
   return (
-    <div className="mainPage">
-     <Navbar />
-     <BusinessPage />
-     <Entrepreneur />
-     <Consultant />
-     <SignUp />
-     <About />
-    </div>
+    <React.Fragment>
+      <Main />
+    </React.Fragment>
+    
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Entrepreneur from './components/Entrepreneur';
+import Consultant from './components/Consultant';
+import SignUp from './components/SignUp';
+import Navbar from './components/Navbar';
+import BusinessPage from './components/BusinessPage';
+import About from './components/About';
+import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+function Main() {
+  return (
+    <div className="mainPage">
+     <Navbar />
+     <BusinessPage />
+     <Entrepreneur />
+     <Consultant />
+     <SignUp />
+     <About />
+    </div>
+  );
+}
+
+export default Main;
