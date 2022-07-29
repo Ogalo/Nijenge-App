ca9e720365dcd78396330c45d269eea76f3c9c67
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-07-29 09:32:54 +0300

    Add the content to ventures component

--- src/components/Entrepreneur.js
+++ src/components/Entrepreneur.js
@@ -2,11 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 
-function Entrepreneur() {
+function Entrepreneur(props) {
+ 
 
-  const handleCancel = ()=>{
 
-  }
 
 
   return (
