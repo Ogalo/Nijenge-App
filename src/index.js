f6832bbb0d60471d9e9b4ca7fff47085367578a3
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-01 03:17:49 +0300

    Add bootstrap CDN

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/style.css";
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
